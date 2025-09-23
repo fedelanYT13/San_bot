ce0227bcb829d92c44eb1f7a1284fe7d4ce9b200
fedelanYT13 <fedelanyt130@gmail.com>
2025-09-23 19:25:16 -0300

    Add files via upload

---
+++ plugins/_welcome.js
@@ -0,0 +1,97 @@
+import { WAMessageStubType } from '@whiskeysockets/baileys';
+
+export async function before(m, { conn, participants, groupMetadata }) {
+  if (!m.messageStubType || !m.isGroup) return true;
+
+  const chat = globalThis.db.data.chats[m.chat];
+  const nombre = globalThis.db.data.users[m.messageStubParameters[0]]?.name || {};
+  const botId = conn.user.jid;
+
+  const ppUrl = await conn.profilePictureUrl(m.messageStubParameters[0], 'image')
+    .catch(() => "https://stellarwa.xyz/files/1752115005119.jpg");
+
+  const name = nombre || conn.getName(m.messageStubParameters[0]);
+  const actionUser = m.key.participant ? await conn.getName(m.key.participant) : null;
+
+  const actionMessages = {
+    [WAMessageStubType.GROUP_PARTICIPANT_ADD]: actionUser ? `\n┊➤ *Agregado por ›* @${m.key.participant.split`@`[0]}` : '',
+    [WAMessageStubType.GROUP_PARTICIPANT_REMOVE]: actionUser ? `\n┊➤ *Eliminado por ›* @${m.key.participant.split`@`[0]}` : '',
+    [WAMessageStubType.GROUP_PARTICIPANT_LEAVE]: ''
+  };
+
+  const userss = m.messageStubParameters[0];
+  const formatText = (template, memberCount) => {
+    return template
+      .replace('@user', `@${userss.split`@`[0]}`)
+      .replace('@group', groupMetadata.subject)
+      .replace('@date', new Date().toLocaleString())
+      .replace('@users', `${memberCount}`)
+      .replace('@type', actionMessages[m.messageStubType])
+      .replace('@desc', groupMetadata.desc?.toString() || '✿ Sin Desc ✿');
+  };
+
+  let memberCount = participants.length;
+  if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) memberCount += 1;
+  else if ([WAMessageStubType.GROUP_PARTICIPANT_REMOVE, WAMessageStubType.GROUP_PARTICIPANT_LEAVE].includes(m.messageStubType)) memberCount -= 1;
+
+const welcomeMessage = formatText(chat.sWelcome || `╭┈──̇─̇─̇────̇─̇─̇──◯◝
+┊「 *Bienvenido (⁠ ⁠ꈍ⁠ᴗ⁠ꈍ⁠)* 」
+┊︶︶︶︶︶︶︶︶︶︶︶
+┊  *Nombre ›* @user
+┊  *Grupo ›* @group
+┊┈─────̇─̇─̇─────◯◝ @type
+┊➤ *Usa /menu para ver los comandos.*
+┊➤ *Ahora somos @users miembros.*
+┊ ︿︿︿︿︿︿︿︿︿︿︿
+╰─────────────────╯`, memberCount);
+
+  const byeMessage = formatText(chat.sBye || `╭┈──̇─̇─̇────̇─̇─̇──◯◝
+┊「 *Hasta pronto (⁠╥⁠﹏⁠╥⁠)* 」
+┊︶︶︶︶︶︶︶︶︶︶︶
+┊  *Nombre ›* @user
+┊┈─────̇─̇─̇─────◯◝ @type
+┊➤ *Ojalá que vuelva pronto.*
+┊➤ *Ahora somos @users miembros.*
+┊ ︿︿︿︿︿︿︿︿︿︿︿
+╰─────────────────╯`, memberCount);
+
+  const leaveMessage = formatText(chat.sBye || byeMessage, memberCount);
+  const mentions = [userss, m.key.participant];
+
+  const fakeContext = {
+    contextInfo: {
+      isForwarded: true,
+      forwardedNewsletterMessageInfo: {
+        newsletterJid: "120363420992828502@newsletter",
+        serverMessageId: '',
+        newsletterName: "₊· ͟͟͞͞꒰✩ 𝐒𝐭𝐞𝐥𝐥𝐚𝐫 𝐖𝐚𝐁𝐨𝐭 - 𝐎𝐟𝐟𝐢𝐜𝐢𝐚𝐥 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 ⏤͟͟͞͞✿"
+      },
+      externalAdReply: {
+        title: namebot,
+        body: dev,
+        mediaUrl: null,
+        description: null,
+        previewType: "PHOTO",
+        thumbnailUrl: icon,
+        sourceUrl: redes,
+        mediaType: 1,
+        renderLargerThumbnail: false
+      },
+      mentionedJid: mentions
+    }
+  };
+
+        if (chat.welcome && m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
+    let caption = welcomeMessage;
+    await conn.sendMessage(m.chat, { image: { url: ppUrl }, caption, ...fakeContext });
+  }
+
+        if (chat.welcome && m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE) {
+    let caption = byeMessage;
+    await conn.sendMessage(m.chat, { image: { url: ppUrl }, caption, ...fakeContext });
+  }
+        if (chat.welcome && m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE) {
+    let caption = welcomeMessage;
+    await conn.sendMessage(m.chat, { image: { url: ppUrl }, caption, ...fakeContext });
+  }
+}
---
+++ plugins/owner-update.js
@@ -0,0 +1,46 @@
+import { execSync } from 'child_process'
+
+var handler = async (m, { conn, text }) => {
+
+try {
+
+const stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''));
+let messager = stdout.toString()
+
+if (messager.includes('🕸 Ya estoy actualizada.')) messager = '🕸 Ya estoy actualizada a la última versión.'
+
+if (messager.includes('🕸 Actualizando.')) messager = '🕸 Procesando, espere un momento mientras me actualizo.\n\n' + stdout.toString()
+conn.reply(m.chat, messager, m)
+
+} catch { 
+try {
+
+const status = execSync('git status --porcelain')
+
+if (status.length > 0) {
+const conflictedFiles = status.toString().split('\n').filter(line => line.trim() !== '').map(line => {
+if (line.includes('.npm/') || line.includes('.cache/') || line.includes('tmp/') || line.includes('datos.json') || line.includes('database.json') || line.includes('sessions/') || line.includes('npm-debug.log')) {
+return null
+}
+return '*→ ' + line.slice(3) + '*'}).filter(Boolean)
+if (conflictedFiles.length > 0) {
+const errorMessage = `🕸 No se puede actualizar.`
+await conn.reply(m.chat, errorMessage, m)
+}
+}
+} catch (error) {
+console.error(error)
+let errorMessage2 = '🐼 Ocurrió un error inesperado.'
+if (error.message) {
+errorMessage2 += '\n🐼 Mensaje de error: ' + error.message;
+}
+await conn.reply(m.chat, errorMessage2, m)
+}
+}
+
+}
+
+handler.command = ['update', 'actualizar']
+handler.owner = true
+
+export default handler
