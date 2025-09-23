f92534aac82edce370f236c5f0d7295441e8ca83
fedelanYT13 <fedelanyt130@gmail.com>
2025-09-23 20:26:09 -0300

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,59 +1,57 @@
-import { WAMessageStubType } from '@whiskeysockets/baileys';
+import { WAMessageStubType} from '@whiskeysockets/baileys';
 
-export async function before(m, { conn, participants, groupMetadata }) {
-  if (!m.messageStubType || !m.isGroup) return true;
+export async function before(m, { conn, participants, groupMetadata}) {
+  if (!m.messageStubType ||!m.isGroup) return true;
 
   const chat = globalThis.db.data.chats[m.chat];
   const nombre = globalThis.db.data.users[m.messageStubParameters[0]]?.name || {};
   const botId = conn.user.jid;
 
   const ppUrl = await conn.profilePictureUrl(m.messageStubParameters[0], 'image')
-    .catch(() => "https://stellarwa.xyz/files/1752115005119.jpg");
+.catch(() => "https://files.catbox.moe/gm249p.jpg");
 
   const name = nombre || conn.getName(m.messageStubParameters[0]);
-  const actionUser = m.key.participant ? await conn.getName(m.key.participant) : null;
+  const actionUser = m.key.participant? await conn.getName(m.key.participant): null;
 
   const actionMessages = {
-    [WAMessageStubType.GROUP_PARTICIPANT_ADD]: actionUser ? `\n┊➤ *Agregado por ›* @${m.key.participant.split`@`[0]}` : '',
-    [WAMessageStubType.GROUP_PARTICIPANT_REMOVE]: actionUser ? `\n┊➤ *Eliminado por ›* @${m.key.participant.split`@`[0]}` : '',
+    [WAMessageStubType.GROUP_PARTICIPANT_ADD]: actionUser? `\n┊➤ *Agregado por ›* @${m.key.participant.split`@`[0]}`: '',
+    [WAMessageStubType.GROUP_PARTICIPANT_REMOVE]: actionUser? `\n┊➤ *Eliminado por ›* @${m.key.participant.split`@`[0]}`: '',
     [WAMessageStubType.GROUP_PARTICIPANT_LEAVE]: ''
-  };
+};
 
   const userss = m.messageStubParameters[0];
   const formatText = (template, memberCount) => {
     return template
-      .replace('@user', `@${userss.split`@`[0]}`)
-      .replace('@group', groupMetadata.subject)
-      .replace('@date', new Date().toLocaleString())
-      .replace('@users', `${memberCount}`)
-      .replace('@type', actionMessages[m.messageStubType])
-      .replace('@desc', groupMetadata.desc?.toString() || '✿ Sin Desc ✿');
-  };
+.replace('@user', `@${userss.split`@`[0]}`)
+.replace('@group', groupMetadata.subject)
+.replace('@date', new Date().toLocaleString())
+.replace('@users', `${memberCount}`)
+.replace('@type', actionMessages[m.messageStubType])
+.replace('@desc', groupMetadata.desc?.toString() || '☕ Sin Desc 🍁');
+};
 
   let memberCount = participants.length;
   if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) memberCount += 1;
   else if ([WAMessageStubType.GROUP_PARTICIPANT_REMOVE, WAMessageStubType.GROUP_PARTICIPANT_LEAVE].includes(m.messageStubType)) memberCount -= 1;
 
-const welcomeMessage = formatText(chat.sWelcome || `╭┈──̇─̇─̇────̇─̇─̇──◯◝
-┊「 *Bienvenido (⁠ ⁠ꈍ⁠ᴗ⁠ꈍ⁠)* 」
-┊︶︶︶︶︶︶︶︶︶︶︶
-┊  *Nombre ›* @user
-┊  *Grupo ›* @group
-┊┈─────̇─̇─̇─────◯◝ @type
-┊➤ *Usa /menu para ver los comandos.*
-┊➤ *Ahora somos @users miembros.*
-┊ ︿︿︿︿︿︿︿︿︿︿︿
-╰─────────────────╯`, memberCount);
+  const welcomeMessage = formatText(chat.sWelcome || `╭───⌁ 𝑲𝒂𝒐𝒓𝒖𝒌𝒐 - 𝑩𝒐𝒕 𝑾𝑨 ⌁───╮
+│ 「 𝑩𝒊𝒆𝒏𝒗𝒆𝒏𝒊𝒅𝒐 𝒂𝒍 𝒈𝒓𝒖𝒑𝒐 」
+│─────────────────────
+│ 🧸 𝑵𝒐𝒎𝒃𝒓𝒆 › @user
+│ 🏷️ 𝑮𝒓𝒖𝒑𝒐 › @group
+│ @type
+│📌 𝑼𝒔𝒂 /menu 𝒑𝒂𝒓𝒂 𝒗𝒆𝒓 𝒍𝒐𝒔 𝒄𝒐𝒎𝒂𝒏𝒅𝒐𝒔.
+│👥 𝑨𝒉𝒐𝒓𝒂 𝒔𝒐𝒎𝒐𝒔 @users 𝒎𝒊𝒆𝒎𝒃𝒓𝒐𝒔.
+╰─────────────────────╯`, memberCount);
 
-  const byeMessage = formatText(chat.sBye || `╭┈──̇─̇─̇────̇─̇─̇──◯◝
-┊「 *Hasta pronto (⁠╥⁠﹏⁠╥⁠)* 」
-┊︶︶︶︶︶︶︶︶︶︶︶
-┊  *Nombre ›* @user
-┊┈─────̇─̇─̇─────◯◝ @type
-┊➤ *Ojalá que vuelva pronto.*
-┊➤ *Ahora somos @users miembros.*
-┊ ︿︿︿︿︿︿︿︿︿︿︿
-╰─────────────────╯`, memberCount);
+  const byeMessage = formatText(chat.sBye || `╭───⌁ 𝑲𝒂𝒐𝒓𝒖𝒌𝒐 - 𝑩𝒐𝒕 𝑾𝑨 ⌁───╮
+│ 「 𝑯𝒂𝒔𝒕𝒂 𝒑𝒓𝒐𝒏𝒕𝒐 」
+│─────────────────────
+│ 🧸 𝑵𝒐𝒎𝒃𝒓𝒆 › @user
+│ @type
+│💭 𝑶𝒋𝒂𝒍𝒂 𝒗𝒖𝒆𝒍𝒗𝒂 𝒑𝒓𝒐𝒏𝒕𝒐.
+│👥 𝑨𝒉𝒐𝒓𝒂 𝒒𝒖𝒆𝒅𝒂𝒏 @users 𝒎𝒊𝒆𝒎𝒃𝒓𝒐𝒔.
+╰─────────────────────╯`, memberCount);
 
   const leaveMessage = formatText(chat.sBye || byeMessage, memberCount);
   const mentions = [userss, m.key.participant];
@@ -62,10 +60,10 @@ const welcomeMessage = formatText(chat.sWelcome || `╭┈──̇─̇─̇─�
     contextInfo: {
       isForwarded: true,
       forwardedNewsletterMessageInfo: {
-        newsletterJid: "120363420992828502@newsletter",
+        newsletterJid: "120363423335018677@newsletter",
         serverMessageId: '',
-        newsletterName: "₊· ͟͟͞͞꒰✩ 𝐒𝐭𝐞𝐥𝐥𝐚𝐫 𝐖𝐚𝐁𝐨𝐭 - 𝐎𝐟𝐟𝐢𝐜𝐢𝐚𝐥 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 ⏤͟͟͞͞✿"
-      },
+        newsletterName: "🌘 𝑴𝒐𝒐𝒏𝒇𝒓𝒂𝒓𝒆 𝒕𝒆𝒂𝒎 ☽"
+},
       externalAdReply: {
         title: namebot,
         body: dev,
@@ -76,22 +74,23 @@ const welcomeMessage = formatText(chat.sWelcome || `╭┈──̇─̇─̇─�
         sourceUrl: redes,
         mediaType: 1,
         renderLargerThumbnail: false
-      },
+},
       mentionedJid: mentions
-    }
-  };
+}
+};
 
-        if (chat.welcome && m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
+  if (chat.welcome && m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
     let caption = welcomeMessage;
-    await conn.sendMessage(m.chat, { image: { url: ppUrl }, caption, ...fakeContext });
-  }
+    await conn.sendMessage(m.chat, { image: { url: ppUrl}, caption,...fakeContext});
+}
 
-        if (chat.welcome && m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE) {
+  if (chat.welcome && m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE) {
     let caption = byeMessage;
-    await conn.sendMessage(m.chat, { image: { url: ppUrl }, caption, ...fakeContext });
-  }
-        if (chat.welcome && m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE) {
+    await conn.sendMessage(m.chat, { image: { url: ppUrl}, caption,...fakeContext});
+}
+
+  if (chat.welcome && m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE) {
     let caption = welcomeMessage;
-    await conn.sendMessage(m.chat, { image: { url: ppUrl }, caption, ...fakeContext });
-  }
+    await conn.sendMessage(m.chat, { image: { url: ppUrl}, caption,...fakeContext});
+}
 }
