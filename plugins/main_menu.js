8970c5ddc3f3d72e68212071e0bff1ecbc715a49
fedelanYT13 <fedelanyt130@gmail.com>
2025-09-24 09:58:30 -0300

    Update main_menu.js

--- plugins/main_menu.js
+++ plugins/main_menu.js
@@ -115,9 +115,6 @@ let handler = async (m, { conn, usedPrefix: _p}) => {
       contextInfo: {
         mentionedJid: [m.sender],
         isForwarded: true,
-        newsletterJid: '120363423335018677@newsletter',
-        newsletterName: '🌘 𝑴𝒐𝒐𝒏𝒇𝒓𝒂𝒓𝒆 𝒕𝒆𝒂𝒎 ☽',
-        serverMessageId: '',
         forwardingScore: 999,
         externalAdReply: {
           title: '☕ 𝑴𝒐𝒐𝒏𝒇𝒓𝒂𝒓𝒆 𝒕𝒆𝒂𝒎 🌙\n⚡︎ 𝑽𝒆𝒓𝒔𝒊𝒐𝒏 𝟐.𝟎.𝟐.𝟓 ☽',
@@ -145,4 +142,4 @@ function clockString(ms) {
   let m = isNaN(ms)? '--': Math.floor(ms / 60000) % 60
   let s = isNaN(ms)? '--': Math.floor(ms / 1000) % 60
   return [h, m, s].map(v => v.toString().padStart(2, '0')).join(':')
-  }
+                     }
