71d83b0775be22398a9fd0ac0198c18f7bc8bad2
fedelanYT13 <fedelanyt130@gmail.com>
2025-09-24 14:21:49 -0300

    Update dow-play.js

--- plugins/dow-play.js
+++ plugins/dow-play.js
@@ -6,7 +6,7 @@ const youtubeRegexID = /(?:youtu\.be\/|youtube\.com\/(?:watch\?v=|embed\/))([a-z
 const handler = async (m, { conn, text, command }) => {
   try {
     if (!text.trim()) {
-      return conn.reply(m.chat, `☕ *_Debes escribir el nombre o link* del video/audio para descargar._&`, m)
+      return conn.reply(m.chat, `☕ *_Debes escribir el nombre o link del video/audio para descargar._*`, m)
     }
 
     await conn.sendMessage(m.chat, { react: { text: "⏳", key: m.key }})
