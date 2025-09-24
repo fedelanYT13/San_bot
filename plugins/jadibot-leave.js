25a403299cf8ccaa09eabe36ec147e27195283b2
fedelanYT13 <fedelanyt130@gmail.com>
2025-09-24 10:29:27 -0300

    Update jadibot-leave.js

--- plugins/jadibot-leave.js
+++ plugins/jadibot-leave.js
@@ -5,17 +5,17 @@ let handler = async (m, { conn, args }) => {
   ].includes(m.sender);
 
   if (!isSocketOwner) {
-    return m.reply('🕸 Solo el propietario del socket puede usar este comando.');
+    return m.reply('☕ Solo el propietario puede usar este comando.');
   }
 
   const groupId = args[0] || m.chat;
 
   try {
-    // await conn.sendMessage(m.chat, { text: `🐼 El bot se despide.` }, { quoted: m });
+    // await conn.sendMessage(m.chat, { text: `🌙 Bye! El bot se despide.` }, { quoted: m });
     await conn.groupLeave(groupId);
   } catch (error) {
     console.error(error);
-    m.reply('🕸 No se pudo abandonar el grupo. Intenta nuevamente.');
+    m.reply('☕ No se pudo abandonar el grupo. Intenta nuevamente.');
   }
 };
 
