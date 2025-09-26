d0727474244299a3d81bd17d53773871c816d2e9
fedelanYT13 <fedelanyt130@gmail.com>
2025-09-26 19:27:39 -0300

    Update _fake.js

--- plugins/_fake.js
+++ plugins/_fake.js
@@ -1,39 +1,56 @@
-export async function before(m, { conn }) {
+export async function before(m, { conn}) {
+  const canal = 'https://whatsapp.com/channel/0029Vb6EMjb6GcGKmVITlG2p'
+  const canal2 = 'https://whatsapp.com/channel/0029Vb6EMjb6GcGKmVITlG2p'
+  const api = 'https://api.moonfare.xyz'
+  const git = 'https://github.com/Dev-fedexyz'
+  const md = 'https:/moonfare.team'
 
-var canal = 'https://whatsapp.com/channel/0029Vb6EMjb6GcGKmVITlG2p'
-var canal2 = 'https://whatsapp.com/channel/0029Vb6EMjb6GcGKmVITlG2p'
-var canal3 = 'https://whatsapp.com/channel/0029Vb6EMjb6GcGKmVITlG2p'
-var api = 'https://api.moonfare.xyz'
-var git = 'https://github.com/Dev-fedexyz'
-var md = 'https:/moonfare.team'
+  globalThis.redes = [canal, api, git, md].getRandom()
 
-globalThis.redes = [canal, canal2, canal3, api, git, md].getRandom()
+  const icon = 'https://cdn.stellarwa.xyz/files/1758842359325.jpeg' // Asegúrate que esta URL exista
 
-  const canales = Object.entries(global.my)
-  .reduce((acc, [key, value]) => {
+  const canales = Object.entries(global.my).reduce((acc, [key, value]) => {
     if (key.startsWith('ch')) {
       const index = key.slice(2)
       const nombre = global.my[`name${index}`]
-      if (nombre) {
-        acc.push({ id: value, nombre })
-      }
-    }
+      if (nombre) acc.push({ id: value, nombre})
+}
     return acc
-  }, [])
+}, [])
 
-const channelRD = canales[Math.floor(Math.random() * canales.length)]
+  const channelRD = canales[Math.floor(Math.random() * canales.length)]
 
-global.fake = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: '0', newsletterName: channelRD.nombre, }, externalAdReply: { showAdAttribution: true, title: packname, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icon, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }}}
-
-global.rcanal = {
-  contextInfo: {
-    isForwarded: true,
-    forwardedNewsletterMessageInfo: {
-      newsletterJid: channelRD.id,
-      newsletterName: channelRD.nombre,
-      serverMessageId: '0'
-    }
-  }
+  global.fake = {
+    contextInfo: {
+      isForwarded: true,
+      forwardedNewsletterMessageInfo: {
+        newsletterJid: channelRD.id,
+        serverMessageId: '0',
+        newsletterName: channelRD.nombre
+},
+      externalAdReply: {
+        showAdAttribution: true,
+        title: global.packname,
+        body: global.dev,
+        mediaUrl: null,
+        description: null,
+        previewType: 'PHOTO',
+        thumbnailUrl: icon,
+        sourceUrl: globalThis.redes,
+        mediaType: 1,
+        renderLargerThumbnail: false
+}
+}
 }
 
+  global.rcanal = {
+    contextInfo: {
+      isForwarded: true,
+      forwardedNewsletterMessageInfo: {
+        newsletterJid: channelRD.id,
+        newsletterName: channelRD.nombre,
+        serverMessageId: '0'
+}
+}
+}
 }
